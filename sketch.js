c5a8b1d90197d0622311ecd4ffca3aac3b540878
Felipe Galdino Pereira dos Santos <82979194+Flpgaldd@users.noreply.github.com>
2025-04-04 20:02:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset= "UTF-8">
+<meta name="viewport" content"width=device-width, initial-scale=1.0"/>
+<meta name="descricao" content="curriculo">
+  <title> Curriculo</title>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="preconnect" href="https://fonts.googleleapis.com"&gt;
+</head>
+  <body>
+    <header>
+      <div class="conteiner">
+        <h1>Felipe Galdino Pereira dos Santos <h1/>
+        <p> Atendente <p/>
+      </div>
+   </header>
+    <main class="conteiner">
+      <section arial-labelledby="sobremim">
+        <h2 id="sobremim">Deus é minha esperança!</h2>
+</body>
+</html> 
